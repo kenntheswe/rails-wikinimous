764286d8cdfe8c6b61a8df20908c832098aaced7
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-05-19 07:09:41 +0900

    Created wikinimous using rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Clearing db"
+Article.destroy_all
+
+puts "Creating articles"
+10.times do
+  Article.create!(
+    title: Faker::Book.title,
+    content: Faker::Quote.famous_last_words
+  )
+end
+puts "Created #{Article.count} articles!"
